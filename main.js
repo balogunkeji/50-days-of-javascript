abe319233ee68a4f5920e31c76623ef1039ddc68
keji <tomitoyo.93@gmail.com>
2025-05-16 18:31:16 +0100

    fix: fund wallet redesign

---
+++ main.js
@@ -0,0 +1,10 @@
+//JavaScript is a high-level programming language
+//JavaScript runs in order.from top to bottom.
+//Reusable code blocks are called functions.
+//Static code shows the same contents
+//dynamic code shows different contents
+//JavaScript was initially created to “make web pages alive.”
+//JavaScript was initially created as a browser-only language, but it is now used in many other environments as well.
+//Today JavaScript has a unique position as the most widely-adopted browser language, fully integrated with HTML/CSS.
+//There are many languages that get “transpiled” to JavaScript and provide certain features. It is recommended to take a look at them, at least briefly, after mastering JavaScript.
+console.log("Hello JavaScript!");
