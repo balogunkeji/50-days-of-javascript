71e238da03b93f34abc5d70f5e056f044495ebaa
keji <tomitoyo.93@gmail.com>
2025-07-22 21:33:35 +0100

    fix: commit

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-//closure
-//variable declared is only visible to the block. surname is not visible outside the block.
-const name = 'Hello Tomi';
+const btn = document.getElementById('btn');
+const result = document.getElementById('result');
+const yearsLeft = document.getElementById('yearsLeft');
+btn.addEventListener('click', () => {
+    const birthYear = document.getElementById('birthYear').value;
+    const currentYear = new Date().getFullYear();
 
-if(name) {
-    let surname = 'Balogun';
-    console.log(name + ' ' + surname);
-}
-
-console.log(name);
-
-//counter
-
-
-function increment() {
-    let counter = 0
-    return function() {
-      let rand = Math.floor(Math.random() * counter++);
-        document.querySelector('span').textContent = rand;
-        localStorage.setItem('count', rand);
-        Number(localStorage.getItem('count'));
+    if (birthYear === '' ||  birthYear > currentYear) {
+        result.textContent = 'Please enter a valid year';
+        yearsLeft.textContent = '';
+        return;
     }
-}
-const count = document.querySelector('button');
-const updateCounter = increment();
-count.addEventListener('click', updateCounter);
-const savedCount = localStorage.getItem('count');
-document.querySelector('span').textContent = savedCount;
+    const age = currentYear - birthYear;
+    result.textContent =`You are ${age} years old.`;
+
+    const remainingYear = 100 - 4;
+    if (remainingYear > 0) {
+        yearsLeft.textContent = `You have ${remainingYear} years left until you turn 100.`;
+    } else {
+        yearsLeft.textContent = `Wow! You turned 100 ${Math.abs(remainingYear)} years ago! 🎉`;
+    }
+})
+
+//variables
+var name = 'Tomisin';
+var name = 'Balogun'
+let names = 'Balogun';
+
+console.log(names, name);
